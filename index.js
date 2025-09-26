4ec69d172c7992f464a17e14f498cf67b5f2e296
WebDevHarsha <vasriharsha@gmail.com>
2025-09-26 16:10:32 +0530

    feat: Add CLI welcome message

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
+import figlet from "figlet";
+import gradient from "gradient-string";
 
+function main(){
+    console.clear();
+    const startMsg=`Welcome     to \n Baseline      CLI`
+
+    figlet(startMsg, (err, data) => {
+        console.log(gradient.pastel.multiline(data))
+    })
+}
+
+main();
